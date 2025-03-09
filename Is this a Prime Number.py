f01eaaf0646c736340c7b68328aae0a306d4d56b
aaravhajare <hajareaarav66@gmail.com>
2025-03-09 09:46:37 +0530

    print(num)

---
+++ Is this a Prime Number.py
@@ -0,0 +1,12 @@
+low = int(input("enter a range"))
+high =int(input("enter a upper rsnge"))
+
+for num in range(low,high + 1) :
+    
+    if num>1 :
+        for i in range(2,num) :
+            if (num % i)==0:
+                break
+
+        else :
+            print(num)
