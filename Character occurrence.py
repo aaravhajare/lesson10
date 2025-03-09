ca0c8700ee75113e3214cda0be96f0f6c289b62e
aaravhajare <hajareaarav66@gmail.com>
2025-03-09 09:35:31 +0530

    Character occurrence

---
+++ Character occurrence.py
@@ -0,0 +1,13 @@
+string = input("enter a string")
+
+char = input("enter a character")
+i = 0
+count = 0
+
+while (i < len(string)):
+
+    if (string[i] == char):
+        count = count + 1
+    i = i+1
+
+print("a character is repeated" , count , "times")
