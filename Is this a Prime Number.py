0e4fe091e44ea3191ecefd231e3907ac3594da0a
aaravhajare <hajareaarav66@gmail.com>
2025-03-15 22:07:56 +0530

    binary

--- Is this a Prime Number.py
+++ Is this a Prime Number.py
@@ -5,7 +5,8 @@ for num in range(low,high + 1) :
     
     if num>1 :
         for i in range(2,num) :
-            if (num % i)==0:
+            if (num % i
+            )==0:
                 break
 
         else :
